bced1d8eddd35644612d3959d5248bc58fbed93a
van-matos <vanmatos@poli.ufrj.br>
2022-07-27 23:39:06 -0300

    Prática com GROUP BY

---
+++ pratica-group-by.sql
@@ -0,0 +1,28 @@
+-- Questão 1
+
+SELECT COUNT ("endDate") AS "currentExperiences"
+FROM experiences;
+
+-- Questão 2
+
+SELECT users.id, COUNT (educations.id) AS "educations"
+FROM educations
+JOIN users
+ON educations."userId" = users.id
+GROUP BY users.id;
+
+-- Questão 3
+
+SELECT users.name AS writer, COUNT (testimonials."writerId") AS testimonialCount
+FROM testimonials
+JOIN users
+ON testimonials."writerId" = users.id
+GROUP BY users.name;
+
+-- Questão 4
+
+SELECT MAX(jobs.salary) AS maximumSalary, roles.name AS role 
+FROM jobs
+JOIN roles
+ON jobs."roleId" = roles.id
+GROUP BY role;
